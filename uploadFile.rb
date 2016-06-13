acc71020c6becff62481143854d8a8b9d98e37f3
JohnTsai <johntsai.work@gmail.com>
2016-06-13 19:44:34 +0800

    add file with ruby script in 2016-06-13 19:44

--- uploadFile.rb
+++ uploadFile.rb
@@ -3,11 +3,13 @@
 
 t = Time.now
 time = t.strftime("%Y-%m-%d %H:%M")
-commit_msg = "add file with ruby script in "+time
+commit_msg = "\"add file with ruby script in #{time}\""
+
+puts commit_msg
 
 system('git status')
 system('git add .')
-system('git commit -am "#{commit_msg}"')
+system("git commit -am #{commit_msg}")
 system('git push')
 
 puts 'upload image with git!'
